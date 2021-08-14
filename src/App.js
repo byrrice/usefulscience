559c93bb46db808f8fc8c8e78dcc9d555c763bc9
byrrice <32532726+byrrice@users.noreply.github.com>
2021-08-14 18:57:16 -0500

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hey hey it'se useful science
         </a>
       </header>
     </div>
